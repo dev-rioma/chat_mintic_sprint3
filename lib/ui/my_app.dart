a63cb5c3de4c71651f84113eb6c5055ebfe1922f
JoalGorod <114000553+JoalGorod@users.noreply.github.com>
2022-12-01 19:49:43 -0500

    Cambio de color en el tema

--- lib/ui/my_app.dart
+++ lib/ui/my_app.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Firebase demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.green,
         ),
         home: const FirebaseCentral());
   }
